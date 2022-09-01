e261391e70a7234fbe14ca3226304fa33dbbe165
Stevanus Christian <stevanuschristian88@gmail.com>
2022-09-01 11:52:26 +0700

    Flutter Snake Game

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
+
 import 'game.dart';
 
 void main() {
@@ -8,38 +7,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Snake Game',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Splash2(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class Splash2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: new HomeScreen(),
-      title: new Text(
-        'Flutter Snake Game',
-        textScaleFactor: 2,
-      ),
-      image: Image.asset('images/snake.png'),
-      loadingText: Text("Loading"),
-      photoSize: 100.0,
-      loaderColor: Colors.blue,
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
